eeaf82fcd4bd4b51cd31743fbc459e588a552e44
Helixlit <felix.burgthaler@gmx.de>
2024-04-29 12:47:43 +0200

    burgi hat gezaubert

---
+++ frontend/utils/waitForElement.js
@@ -0,0 +1,16 @@
+export default function waitForElementToDisplay(selector, callback, checkFrequencyInMs, timeoutInMs) {
+  var startTimeInMs = Date.now();
+  (function loopSearch() {
+    if (document.querySelector(selector) != null) {
+      callback();
+      return;
+    }
+    else {
+      setTimeout(function () {
+        if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs)
+          return;
+        loopSearch();
+      }, checkFrequencyInMs);
+    }
+  })();
+}
