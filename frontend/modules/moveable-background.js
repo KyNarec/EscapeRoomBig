4c3d652bd9ce1c9a94f592b3debbbdf305a26c19
dauerndamprogramieren <haeusslerludwig@gmail.com>
2024-05-06 20:49:53 +0200

    This way it should work but it does not

--- frontend/modules/moveable-background.js
+++ frontend/modules/moveable-background.js
@@ -1,23 +1,34 @@
+//Make the DIV element draggagle:
 dragElement(document.getElementById("moveable-image"));
 
 function dragElement(elmnt) {
-    var pos3 = 0;
-    elmnt.onmousedown = dragMouseDown;
-    function dragMouseDown(e) {
-      e = e || window.event;
-      pos3 = e.clientX;
-      document.onmouseup = closeDragElement;
-      document.onmousemove = elementDrag;
-    }
-    function elementDrag(e) {
-      e = e || window.event;
-      var pos1 = pos3 - e.clientX;
-      pos3 = e.clientX;
-      elmnt.style.left = (elmnt.offsetLeft + pos1) + "px";
-      elmnt.style.top = (elmnt.offsetTop + pos1) + "px";
-    }
-    function closeDragElement() {
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
   }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos3 = e.clientX;
+    // set the element's new position:
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
