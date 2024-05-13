dc5e81f39f49f205c50370629990e0c9013fda43
dauerndamprogramieren <haeusslerludwig@gmail.com>
2024-05-13 12:46:13 +0200

    more working

--- frontend/core/app.js
+++ frontend/core/app.js
@@ -2,4 +2,10 @@ import LandingPage from "../layouts/landing_page.js";
 import Component from "../modules/Component.js";
 import {Scenes}  from "../utils/scenes_enum.js"
 
-const landing_page = new LandingPage();
+const main = await document.querySelector('main');
+
+const header = new Component("header");
+const testSzene01 = new Component("Szene_01_Gasse/index");
+
+await header.appendTo(main);
+await testSzene01.appendTo(main);
