79f424d57b663471e7cc2bfeff2b9857e89d6a50
Helixlit <felix.burgthaler@gmx.de>
2024-05-13 12:54:40 +0200

    asdfa

--- frontend/core/app.js
+++ frontend/core/app.js
@@ -2,4 +2,16 @@ import LandingPage from "../layouts/landing_page.js";
 import Component from "../modules/Component.js";
 import {Scenes}  from "../utils/scenes_enum.js"
 
-const landing_page = new LandingPage();
+const main = await document.querySelector('main');
+
+const header = new Component('header')
+const scene2 = new Component('Szene_02_Daheim/index');
+await scene2.appendTo(document.querySelector('main'));
+const screen_interact = document.querySelector('#screen-interact');
+
+const screen = new Component('screen');
+
+screen_interact.onclick = () => {
+    header.appendTo(main);
+    alert('adf');
+}
---
+++ frontend/js/main.js
@@ -0,0 +1,14 @@
+var i = 0;
+function newButton(){
+var button = document.createElement('button');
+      button.textContent = 'Rätsel ' + i;
+      button.setAttribute("type", "button");
+      button.setAttribute("class", "btn btn-primary m-1 flex-grow-1");
+      i++;
+      var div = document.getElementById('progress-buttons');
+      div.appendChild(button);
+}
+
+// um auf die verschiedenen rätsel zu springen kann man ja wieder
+// ein Attribute setten und dann als type onclick machen und mit ner variable
+// auf ne website verweisen
