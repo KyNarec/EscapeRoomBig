9d42bd7035d5157ca84ba500953d83f9257fc8c7
Helixlit <felix.burgthaler@gmx.de>
2024-06-03 11:54:59 +0200

    df

--- frontend/core/app.js
+++ frontend/core/app.js
@@ -1,17 +1,12 @@
-import LandingPage from "../layouts/landing_page.js";
 import Component from "../modules/Component.js";
-import {Scenes}  from "../utils/scenes_enum.js"
+
+
 
 const main = await document.querySelector('main');
 
-const header = new Component('header')
-const scene2 = new Component('Szene_02_Daheim/index');
-await scene2.appendTo(document.querySelector('main'));
-const screen_interact = document.querySelector('#screen-interact');
 
-const screen = new Component('screen');
+const header = new Component("header");
+const scene2 = new Component("Szene_02_Daheim/index");
 
-screen_interact.onclick = () => {
-    header.appendTo(main);
-    alert('adf');
-}
+await header.appendTo(main);
+scene2.appendTo(main);
