e0599138349237c00f33c023bd08ba72a7766774
dauerndamprogramieren <haeusslerludwig@gmail.com>
2024-06-10 17:08:04 +0200

    Szene_01_Gasse streetview done

--- frontend/components/Szene_01_Gasse/index.js
+++ frontend/components/Szene_01_Gasse/index.js
@@ -32,14 +32,45 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-  var position = 0;
+  var position = 2;
   const pictures = ["Theresienstraße_1.jpg","Theresienstraße_2.jpg","Theresienstraße_3.jpg","Kreuzung_Theresienstraße.jpg","Theresienstraße_4.jpg","Theresienstraße_5.jpg","Dietrich-Bonhoeffer-Platz_1.jpg","Dietrich-Bonhoeffer-Platz_2.jpg","Dietrich-Bonhoeffer-Platz_3.jpg","Dietrich-Bonhoeffer-Platz_4.jpg","Dietrich-Bonhoeffer-Platz_5.jpg","Dietrich-Bonhoeffer-Platz_6.jpg","Rosengasse_3.jpg","Rosengasse_2.jpg","Rosengasse_1.jpg"];
-  const ButPosX = ["63%","60%","61%","82%","20%","50%","48%","40%","65%","60%","60%","60%","200%","40%"];  
+  const ButPosFX = ["63%","60%","61%","82%","20%","50%","48%","40%","65%","60%","60%","200%","17%","17%","200%"]; 
+  const ButPosBX = ["","15%","15%","39%","64%","19%","23%","10%","22%","20%","18%","15%","","60%","70%"];
   var buttonf = document.getElementById("forward");
     buttonf.onclick=()=>{
           position = position+1;
-          let pic = pictures[position];
-          var change = "/frontend/assets/images/Szene01/" + pic;
-          document.getElementById("background").src=change;
-          buttonf.style.marginLeft=ButPosX[position];
+          intersectionButton(position);
     }
+
+  var buttonb = document.getElementById("backward");
+  buttonb.onclick=()=>{
+    position = position-1;
+    intersectionButton(position);
+  }
+
+  var buttoni = document.getElementById("inter");
+  buttoni.onclick=()=>{
+    if(position == 3){
+      position = 12;
+    }else{
+      position = 3;
+    }
+    intersectionButton(position);
+  }
+
+  function intersectionButton(pos){
+    let pic = pictures[pos];
+    var change = "/frontend/assets/images/Szene01/" + pic;
+    document.getElementById("background").src=change;
+    buttonf.style.marginLeft=ButPosFX[pos];
+    buttonb.style.marginLeft=ButPosBX[pos];
+    if(pos == 3 || pos == 12){
+      if(pos == 3){
+        buttoni.style.marginLeft="19%";
+      }else{
+        buttoni.style.marginLeft="60%"
+      }
+    }else{
+      buttoni.style.marginLeft="";
+    }
+  }
