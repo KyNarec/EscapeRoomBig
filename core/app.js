5af6182ace5ae8fb92c6b58b1298521b9c512087
S211054 <jim48@gmx.de>
2024-06-17 12:53:05 +0200

    wir haben im main.js gezaubert (maybe)

--- core/app.js
+++ core/app.js
@@ -23,12 +23,40 @@ document.body.addEventListener("mousedown", (e) => {
     if(bCloseModals) openModals.forEach((modal) => { modal.remove(); });
 });
 
-await waitForElm('#area-open-pc');
-const areaOpenPc = document.querySelector('#area-open-pc');
-areaOpenPc.addEventListener("click", () => {
-    const pc = document.createElement('custom-comp');
-    pc.setAttribute('name', 'computer');
-    pc.classList.add('modal');
-    document.body.appendChild(pc);
-    openModals.push(pc);
+// await waitForElm('#area-open-pc');
+// const areaOpenPc = document.querySelector('#area-open-pc');
+// areaOpenPc.addEventListener("click", () => {
+//     const pc = document.createElement('custom-comp');
+//     pc.setAttribute('name', 'computer');
+//     pc.classList.add('modal');
+//     document.body.appendChild(pc);
+//     openModals.push(pc);
+// });
+
+await waitForElm('#area-open-tuer');
+const areaOpenTuer = document.querySelector('#area-open-tuer');
+areaOpenTuer.addEventListener("click", () => {
+    const schloss = document.createElement('custom-comp');
+    schloss.setAttribute('name', 'schloss');
+    schloss.classList.add('modal');
+    document.body.appendChild(schloss);
+    openModals.push(schloss);
+    const blur = document.createElement('div');
+    // blur.style = "backdrop-filter: blur(10px); z-index: 100; background-color: black; width: 100vw; height: 100vh; position: absolute";
+    blur.style = "backdrop-filter: blur(10px); background-color: black; width: 100vw; height: 100vh; display: block; position: absolute";
+    blur.id = 'computer-container'
+    blur.classList.add('modal');
+    document.body.appendChild(blur);
 });
+
+await waitForElm('#area-open-kreuz');
+const areaOpenKreuz = document.querySelector('#area-open-kreuz');
+areaOpenKreuz.addEventListener("click", () => {
+    const kreuz = document.createElement('custom-comp');
+    kreuz.setAttribute('name', 'kreuz');
+    kreuz.classList.add('modal');
+    document.body.appendChild(kreuz);
+    openModals.push(kreuz);
+});
+
+//schwarzes bild ist immernoch unten
